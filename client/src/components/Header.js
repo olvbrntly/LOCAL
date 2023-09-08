c5cd17e2f8c72f9a2dfa920674d5892d5a901f5c
olivia <olivia.brantley9@gmail.com>
2023-09-08 13:22:07 -0700

    created header component

---
+++ client/src/components/Header.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import '../index.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
+
+const logo = require('./images/logo.png')
+
+const Header = () =>{
+    return(
+        <div className='header'>
+            <div className='logo'>
+                <img src={logo} alt='LOCAL logo' className='logo'></img>
+            </div>
+
+            <div className='header-right'>
+                <a href='http://localhost:3000/'>Locations</a>
+                <a href='http://localhost:3000/'>Categories</a>
+                <a href='http://localhost:3000/'>Login / Sign Up</a>
+                <button className='user-button'>
+                    <FontAwesomeIcon icon={faUser}/>
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default Header
