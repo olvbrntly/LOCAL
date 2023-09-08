dc6731195406dc56b13636012c78b055491d1706
olivia <olivia.brantley9@gmail.com>
2023-09-08 10:14:09 -0700

    created basic Address model

--- server/models/Address.js
+++ server/models/Address.js
@@ -2,6 +2,22 @@ const mongoose = require('mongoose');
 
 const addressSchema = new mongoose.Schema(
     {
-        
+        buildingNum:{
+            type:String,
+        },
+        streetName:{
+            type:String,
+        },
+        city:{
+            type:String,
+        },
+        state:{
+            type:String,
+        },
+        zip:{
+            type:String
+        }
     }
 )
+
+module.exports = mongoose.model('Address', addressSchema)
