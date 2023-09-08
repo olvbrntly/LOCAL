90b80a7827546920985268ac4c50e6c54d1ca95c
olivia <olivia.brantley9@gmail.com>
2023-09-07 19:57:29 -0700

    message

--- server/config/DbConnection.js
+++ server/config/DbConnection.js
@@ -1,11 +1,12 @@
-const mongoose = require(mongoose);
+const mongoose = require('mongoose');
 
 const connectDB = async () =>{
     try{
         await mongoose.connect(process.env.DATABASE_URI);
+        console.log('connected to mongodb!')
     } catch(err){
         console.log(err);
     }
 }
 
-module.export = connectDB;
+module.exports = connectDB;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,11 @@
+require('dotenv').config()
+
 const express = require('express');
+
 const path = require('path');
 const cookieParser = require('cookie-parser');
-const connectDB = reqwuire('./config/dbConnection');
+const connectDB = require('./config/DbConnection');
 const mongoose = require('mongoose');
 const PORT = process.env.PORT || 3500
+
+connectDB()
