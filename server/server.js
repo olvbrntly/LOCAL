145c7285c37866c78eb6913e422e470b23bf7e3f
olivia <olivia.brantley9@gmail.com>
2023-09-08 09:57:21 -0700

    added to basic express setup

--- server/server.js
+++ server/server.js
@@ -1,11 +1,23 @@
 require('dotenv').config()
 
 const express = require('express');
+const app = express();
 
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const connectDB = require('./config/DbConnection');
 const mongoose = require('mongoose');
+const { appendFile } = require('fs');
 const PORT = process.env.PORT || 3500
 
-connectDB()
+connectDB();
+
+
+// lets app recieve and parse json data
+app.use(express.json())
+
+//3rd part middleware
+app.use(cookieParser());   
+
+app.use('/', express.static(path.join(__dirname, '/public')));
+
