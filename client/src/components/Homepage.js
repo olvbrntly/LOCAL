8fb6e12ec5d54c8cd17bff262e4c5811483ac840
olivia <olivia.brantley9@gmail.com>
2023-09-08 14:54:41 -0700

    created homepage component- hero img and text

---
+++ client/src/components/Homepage.js
@@ -0,0 +1,15 @@
+const Homepage = () =>{
+    return(
+        <div className="hero">
+            <div className="hero-content">
+
+                
+                <h1 className="hero-title">Shop LOCAL</h1>
+                <p className="hero-text">Invest in your community. Find small, family owned, and local businesses near you.</p>
+
+            </div>
+        </div>
+    )
+}
+
+export default Homepage;
