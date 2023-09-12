ac3cc607ab7fd1864261c94b6a06d60401051585
olivia <olivia.brantley9@gmail.com>
2023-09-12 14:03:55 -0700

    removed console.log

--- server/config/DbConnection.js
+++ server/config/DbConnection.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const connectDB = async () =>{
     try{
         await mongoose.connect(process.env.DATABASE_URI);
-        console.log('connected to mongodb!')
     } catch(err){
         console.log(err);
     }  
