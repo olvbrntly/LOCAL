4413481a72a24b1030fe7f300e04caae091f45f6
olivia <olivia.brantley9@gmail.com>
2023-09-11 18:34:40 -0700

    placeholder terms component

---
+++ client/src/components/Terms.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+
+const Terms = () =>{
+    return(
+    <div className="content terms">
+        <h1>Terms and Conditions</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas lacinia, neque nec tincidunt dignissim, elit mi vestibulum arcu, ut lacinia velit metus nec ipsum. Phasellus egestas sem eget pharetra posuere. Etiam ultrices odio iaculis congue tincidunt. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed sed augue tristique tellus dapibus viverra in id nibh. Nam ac lectus nec nunc porta bibendum. Phasellus accumsan consequat sapien, vel maximus quam finibus at. Morbi eu libero ut nisi ultricies pellentesque non at tortor.
+        </p>
+
+        <p>
+        urabitur non egestas mauris. Fusce gravida lacus id enim laoreet sollicitudin. Praesent cursus, enim eget convallis vestibulum, ex dolor tempus velit, ut sagittis felis nunc a odio. Quisque dictum vehicula nisi, in mattis sem. Integer id nibh id augue vehicula porttitor. Morbi condimentum egestas luctus. Curabitur vitae mollis lacus. Aliquam non justo sit amet orci ultricies viverra ac eu velit.
+        </p>
+
+        <p>
+            In id consectetur nunc. Duis a tempus quam. Phasellus imperdiet ullamcorper metus, sed aliquam mi. Donec pretium rhoncus dignissim. Maecenas pretium tempus libero sit amet tempor. Ut et velit ligula. Ut id purus interdum, ultricies arcu in, rhoncus mi. Donec rhoncus porta fermentum. Duis scelerisque bibendum enim, eu pellentesque neque ullamcorper ut. Nulla facilisi. Curabitur sagittis dui volutpat, euismod dolor vel, semper mauris. Nunc aliquam aliquet mauris, ut malesuada leo pellentesque at.Sed massa neque, porta eget lacinia vel, posuere vitae nulla. Nulla ut fermentum quam, non efficitur nulla. Etiam id posuere justo, eu bibendum nulla. Ut nibh est, consequat quis convallis nec, commodo quis ipsum. Morbi at lobortis est. Donec nec tellus sit amet orci iaculis convallis ut vel ipsum. Nullam sed cursus massa. Etiam tristique accumsan urna, at bibendum urna tempor ut. Duis sed felis metus.
+        </p>
+
+    </div>
+    )
+}
+
+export default Terms
