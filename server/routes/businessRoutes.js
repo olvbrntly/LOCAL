56e71ac48415ed6614290ce397d55c591609c573
olivia <olivia.brantley9@gmail.com>
2023-09-12 14:03:22 -0700

    added business routes

---
+++ server/routes/businessRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+const businessController = require('../controllers/businessController')
+
+router.route('/')
+    .get(businessController.getAllBusinesses)
+    // .post(businessController.createNewBusiness)
+    // .patch(businessController.editBusiness)
+    // .delete(businessController.deleteBusiness)
+
+module.exports = router
