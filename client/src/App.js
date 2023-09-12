80c59bce9296ef7f94e0a391a141d417b832f205
olivia <olivia.brantley9@gmail.com>
2023-09-11 18:21:50 -0700

    added apbout path

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import Header from './components/Header'
 import Footer from './components/Footer';
 import { BrowserRouter as  Router, Route, Routes} from 'react-router-dom';
 import Homepage from './components/Homepage'
+import About from './components/About'
 
 const  App  =() => {
   return (
@@ -10,6 +11,7 @@ const  App  =() => {
       <Router>
         <Routes>
           <Route path='/' element={<Homepage/>}/>
+          <Route path='/about' element={<About/>}/>
         </Routes>
       </Router>
       <Footer />
