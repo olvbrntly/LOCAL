225f2f070d6baa6bc0e2cde97916c6ff4d24e10f
olivia <olivia.brantley9@gmail.com>
2023-09-11 18:00:01 -0700

    added footer class

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebook, faInstagram, faPinterest, faTiktok, faTwitter} from '@fortawesome/free-brands-svg-icons';
 const Footer = () => {
     return(
-        <div>
+        <div className='footer'>
             <div className='footer-links'>
                 <a href='http://localhost:3000/'>About</a>
                 <a href='http://localhost:3000/'>Contact</a>
