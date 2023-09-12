842e2e425852d0dfce63697ecf821247940ad6da
olivia <olivia.brantley9@gmail.com>
2023-09-11 18:35:24 -0700

    added terms and contact route

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
-import Header from './components/Header'
+import Header from './components/Header';
 import Footer from './components/Footer';
 import { BrowserRouter as  Router, Route, Routes} from 'react-router-dom';
 import Homepage from './components/Homepage'
-import About from './components/About'
+import About from './components/About';
+import Terms from './components/Terms';
+import Contact from './components/Contact';
 
 const  App  =() => {
   return (
@@ -12,6 +14,8 @@ const  App  =() => {
         <Routes>
           <Route path='/' element={<Homepage/>}/>
           <Route path='/about' element={<About/>}/>
+          <Route path='/terms' element={<Terms/>}/>
+          <Route path='/contact' element={<Contact/>}/>
         </Routes>
       </Router>
       <Footer />
