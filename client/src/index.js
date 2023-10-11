3e320c8358528300ee7c538ccc3ceafa929a545b
olivia <olivia.brantley9@gmail.com>
2023-10-11 15:38:54 -0700

    removed second router

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter, Routes,Route } from 'react-router-dom';
+
 
 import { store } from './app/store';
 import { Provider } from 'react-redux';
@@ -11,11 +11,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/*' element={<App />} />
-        </Routes>
-      </BrowserRouter>
+     
+        <App />
+     
     </Provider>
   </React.StrictMode>
 );
