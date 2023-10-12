9c476586e00844820fc54586a7c1662d240a700d
olive <olivia.brantley9@gmail.com>
2023-10-11 20:24:49 -0700

    got rid of whitespace

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { apiSlice } from './api/apiSlice'
 
+
 export const store = configureStore({
     reducer:{
         [apiSlice.reducerPath]:apiSlice.reducer
