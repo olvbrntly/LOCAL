7dd6e764b808cfe196b414359683c83d174c8115
olive <olivia.brantley9@gmail.com>
2023-10-13 16:36:14 -0700

    added admin to path

--- client/src/App.js
+++ client/src/App.js
@@ -20,7 +20,7 @@ const  App  =() => {
           <Route path='/contact' element={<Contact/>}/>
 
           <Route path='business' element={<BusinessesList/>}/>
-          <Route path='business/create' element={<AddBusinessForm/>}/>
+          <Route path='business/admin/create' element={<AddBusinessForm/>}/>
 
         </Routes>
       </Router>
