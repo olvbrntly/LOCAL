85f1c1d4a1cb08f56a4c95ea525d711b384ae94d
olive <olivia.brantley9@gmail.com>
2023-10-13 10:32:14 -0700

    added very basic contact page

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -3,8 +3,15 @@ import React from "react";
 
 const Contact = () =>{
     return(
-        <div className="content">
-            Content!
+        <div className="content contact">
+            <h1>We Would Love To Hear From You!</h1>
+
+            <div className="contact-info">
+          
+                <h4>Contact Us</h4>
+                <p>(555)-555-555</p>
+                <p>customer@local.com</p>
+            </div>
         </div>
     )
 }
