0eeb5ac9d09214fa0afeb5ca57ec4b4ddd7aa822
olive <olivia.brantley9@gmail.com>
2023-10-13 11:11:02 -0700

    cosmetic edits

--- client/src/features/Business/BusinessCard.js
+++ client/src/features/Business/BusinessCard.js
@@ -2,37 +2,32 @@
 import { selectBusinessById } from "./businessSlice";
 import { useSelector} from "react-redux";
 import Card from 'react-bootstrap/Card';
-import ListGroup from 'react-bootstrap/ListGroup';
+// import ListGroup from 'react-bootstrap/ListGroup';
 
 //import { useNavigate } from "react-router-dom";
 
 
 const BusinessCard = ({businessId}) =>{
     const business = useSelector(state => selectBusinessById(state, businessId));
-
-   
-
     if(business) {
 
-    
-
     return (
-        <Card style={{ width: '18rem', borderRadius:'25px'}}>
+        <Card style={{ width: '18rem', borderRadius:'25px' ,textAlign:"center"}}>
           {/* <Card.Img variant="top" src="holder.js/100px180?text=Image cap" /> */}
           <Card.Body>
             <Card.Title>{business.name}</Card.Title>
             <Card.Text> {business.priceRange}</Card.Text>
             <Card.Text>
                 {/* will change to a tag line */}
-             {business.description.substring(0,100)} 
+             {business.description.substring(0,200)} 
             </Card.Text>
           </Card.Body>
-          <ListGroup className="list-group-flush">
+          {/* <ListGroup className="list-group-flush">
             <ListGroup.Item>Email</ListGroup.Item>
             <ListGroup.Item>Phone Number </ListGroup.Item>
-          </ListGroup>
+          </ListGroup> */}
           <Card.Body>
-          <ListGroup.Item>Categories: Grocery </ListGroup.Item>
+          {/* <ListGroup.Item>Categories: Grocery </ListGroup.Item> */}
             <Card.Link href="#">Link to Business Info page </Card.Link>
           </Card.Body>
         </Card>
