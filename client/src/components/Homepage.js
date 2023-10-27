9836e10e629affeeea08921f4365f2a23b0158b5
olivia <olivia.brantley9@gmail.com>
2023-10-27 10:40:55 -0700

    added zip code search input( not functional yet)

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,3 +1,7 @@
+import { FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from"@fortawesome/free-solid-svg-icons";
+
+
 const Homepage = () =>{
     return(
         <div className=" hero content">
@@ -6,6 +10,10 @@ const Homepage = () =>{
                 
                 <h1 className="hero-title">Shop LOCAL</h1>
                 <p className="hero-text">Invest in your community. Find small, family owned, and local businesses near you.</p>
+                <form>
+                <input id="zip" name="zip" type="text" pattern="[0-9]*" placeholder="Zip Code"/>
+                <button className='homepage-button'><FontAwesomeIcon icon={faMagnifyingGlass} /></button>
+                </form>
 
             </div>
         </div>
