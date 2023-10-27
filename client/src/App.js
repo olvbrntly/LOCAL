4b55b679e5fa72a6cfb7ffa8a03d1b0ab8993c5d
olive <olivia.brantley9@gmail.com>
2023-10-27 15:17:38 -0700

    added link to admin home page

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,9 @@ import Terms from './components/Terms';
 import Contact from './components/Contact';
 import BusinessesList from './features/Business/BusinessesList';
 import AddBusinessForm from './features/Business/AddBusinessForm';
-import AdminBusinessList from './features/Business/AdminBusinessList';
+import AdminBusinessList from './features/Admin/AdminBusinessList';
+import AdminHome from './features/Admin/AdminHome';
+
 
 const  App  =() => {
   return (
@@ -15,14 +17,21 @@ const  App  =() => {
       <Header />
       <Router>
         <Routes>
+         
+        {/* Homepage */}
           <Route path='/' element={<Homepage/>}/>
+
+        {/* Footer Routes */}
           <Route path='/about' element={<About/>}/>
           <Route path='/terms' element={<Terms/>}/>
           <Route path='/contact' element={<Contact/>}/>
 
+        {/* User Routes */}
           <Route path='business' element={<BusinessesList/>}/>
-          <Route path='admin/businesses' element={<AdminBusinessList/>}/>
 
+        {/* Admin Routes */}
+          <Route path='admin' element={<AdminHome/>}/>
+          <Route path='admin/businesses' element={<AdminBusinessList/>}/>
           <Route path='admin/business/create' element={<AddBusinessForm/>}/>
 
         </Routes>
