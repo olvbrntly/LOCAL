ee80c97605d2b39f7251abb394b12f6f3159f7f8
olive <olivia.brantley9@gmail.com>
2023-10-27 12:51:03 -0700

    form submit works

--- client/src/features/Business/AddBusinessForm.js
+++ client/src/features/Business/AddBusinessForm.js
@@ -1,5 +1,6 @@
 import React from "react"
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import { useNavigate } from "react-router-dom"
 // import { useSelector } from "react-redux"
 import { useAddBusinessMutation } from "./businessSlice"
 import Button from 'react-bootstrap/Button';
@@ -9,33 +10,52 @@ import Row from 'react-bootstrap/Row';
 
 const AddBusinessForm = () =>{
 
+  const navigate = useNavigate();
+
     const [name, setName]= useState('');
     const [description, setDescription] = useState('')
+    const [tagline, setTagline] = useState('')
 
-    const [addBusiness,{isLoading}]  = useAddBusinessMutation()
+    const [addBusiness,
+          {isLoading,
+            isSuccess,
+            isError,
+            error
+            
+    }]  = useAddBusinessMutation()
 
     const onNameChanged = e => setName(e.target.value);
     const onDescriptionChanged = e => setDescription(e.target.value);
+    const onTaglineChanged = e => setTagline(e.target.value);
 
-    const canSave = [name,description].every(Boolean) && !isLoading;
+    const canSave = [name, description, tagline].every(Boolean) && !isLoading;
 
-    const onSaveBusinessClicked = async () =>{
-        if(canSave){
-            try{
-                await addBusiness({name, description}).unwrap();
-                setName('');
-                setDescription('')
-            }catch(err){
-                console.error('Failed to save business', err)
-            }
-        }
+
+    const onSaveBusinessClicked = async (e) =>{
+            e.preventDefault()
+            if(canSave){
+              await addBusiness({ name, description, tagline })
+          }
     }
 
+    useEffect(() => {
+      if (isSuccess) {
+          setName('')
+          setDescription('')
+          setTagline('')
+          navigate('/business')
+      }
+  }, [isSuccess, navigate])
+
+  const errClass = isError ? "errmsg" : "offscreen"
 
     return(
+      
       <div className="add-business-form">
       <h1>Add a New Business</h1>
-          <Form>
+      <p className={errClass}>{error?.data?.message}</p>
+
+          <Form onSubmit={onSaveBusinessClicked}>
           <Row className="mb-3">
           <Form.Group as={Col} controlId="formGridName">
             <Form.Label>Name</Form.Label>
@@ -52,7 +72,7 @@ const AddBusinessForm = () =>{
         <Row className="mb-3">
           <Form.Group as={Col} controlId="formGridTagline">
             <Form.Label>Tag Line</Form.Label>
-            <Form.Control type="text" placeholder="Description of business in 100 characters or less" maxLength="100" />
+            <Form.Control type="text" placeholder="Description of business in 100 characters or less" maxLength="100"  onChange={onTaglineChanged}/>
           </Form.Group>
         </Row>
 
@@ -104,7 +124,7 @@ const AddBusinessForm = () =>{
           </Form.Group>
         </Row>
 
-        <Button variant="primary" type="submit" onClick={onSaveBusinessClicked}>
+        <Button variant="primary" type="submit" >
           Save Business
         </Button>
       </Form>
