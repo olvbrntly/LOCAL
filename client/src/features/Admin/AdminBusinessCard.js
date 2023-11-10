f84333d5006656d4fbb425923011d7d552450b8d
olivia <olivia.brantley9@gmail.com>
2023-11-10 11:41:34 -0800

    cleaned up admin cards, eliminated reused code

--- client/src/features/Admin/AdminBusinessCard.js
+++ client/src/features/Admin/AdminBusinessCard.js
@@ -1,8 +1,8 @@
 import './admin.css'
 import { selectBusinessById } from "../Business/businessSlice";
 import { useSelector} from "react-redux";
-import Card from 'react-bootstrap/Card';
 import { useNavigate } from "react-router-dom";
+import BusinessCard from '../Business/BusinessCard';
 
 const AdminBusinessCard = ({businessId}) =>{
   const navigate = useNavigate();
@@ -16,26 +16,9 @@ const AdminBusinessCard = ({businessId}) =>{
 
     return (
         <div className="admin-business-card">
-        <Card style={{ width: '18rem', borderRadius:'25px' ,textAlign:"center", height:'300px'}}>
-          {/* <Card.Img variant="top" src="holder.js/100px180?text=Image cap" /> */}
-          <Card.Body>
-            <Card.Title>{business.name}</Card.Title>
+          <BusinessCard businessId={business.id}/>
 
-            <Card.Text>
-                {/* will change to a tag line */}
-             {business.description.substring(0,200)} 
-            </Card.Text>
-          </Card.Body>
-          {/* <ListGroup className="list-group-flush">
-            <ListGroup.Item>Email</ListGroup.Item>
-            <ListGroup.Item>Phone Number </ListGroup.Item>
-          </ListGroup> */}
-          <Card.Body>
-          {/* <ListGroup.Item>Categories: Grocery </ListGroup.Item> */}
-            <Card.Link >Link to Business Info page </Card.Link>
-          </Card.Body>
-        </Card>
-        <button onClick={onClickEditBusiness} >Edit Business</button>
+          <button onClick={onClickEditBusiness} >Edit</button>
         </div>
         
       );}else return null
