3326d0fa88ac13eff6a521f5c96240411dd897c4
olivia <olivia.brantley9@gmail.com>
2023-11-15 18:47:00 -0800

    added setuplisteners

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { apiSlice } from './api/apiSlice'
-
+import { setupListeners } from "@reduxjs/toolkit/query"
 
 export const store = configureStore({
     reducer:{
@@ -9,3 +9,5 @@ export const store = configureStore({
     middleware: getDefaultMiddleware => getDefaultMiddleware().concat(apiSlice.middleware),
     devTools:true
 })
+
+setupListeners(store.dispatch)
