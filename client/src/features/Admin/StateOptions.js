73799f03d7269f8915d685b03cd58d71217fb155
olivia <olivia.brantley9@gmail.com>
2024-01-02 11:46:37 -0800

    component for state select box

---
+++ client/src/features/Admin/StateOptions.js
@@ -0,0 +1,74 @@
+import Form from 'react-bootstrap/Form';
+
+
+
+const StateOptions = () =>{
+
+    const options =[
+        'AL',
+        'AK',
+        'AZ',
+        'AR',
+        'CA',
+        'CO',
+        'CT',
+        'DE',
+        'DC',
+        'FL',
+        'GA',
+        'HI',
+        'ID',
+        'IL',
+        'IN',
+        'IA',
+        'KS',
+        'KY',
+        'LA',
+        'ME',
+        'MD',
+        'MA',
+        'MI',
+        'MN',
+        'MS',
+        'MO',
+        'NE',
+        'NV',
+        'NH',
+        'NJ',
+        'NM',
+        'NY',
+        'NC',
+        'ND',
+        'OH',
+        'OK',
+        'OR',
+        'PA',
+        'RI',
+        'SC',
+        'SD',
+        'TN',
+        'TX',
+        'UT',
+        'VT',
+        'VI',
+        'VA',
+        'WA',
+        'WV',
+        'WI',
+        'WY'
+    ]
+    return(
+        <>
+            <Form.Label>State</Form.Label>
+            <Form.Select defaultValue="...">
+            <option disabled selected value> -- select an option -- </option>
+            {options.map((option) =>(
+                <option key={option}>{option}</option>
+            ))}
+            </Form.Select>
+        </>
+    )
+ 
+}
+
+export default StateOptions
