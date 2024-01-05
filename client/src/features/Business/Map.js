87d024da659697a436877f94ef1b9b24e0675548
olivia <olivia.brantley9@gmail.com>
2024-01-04 16:20:35 -0800

    map component embeds google map using lng, lat

--- client/src/features/Business/Map.js
+++ client/src/features/Business/Map.js
@@ -1,5 +1,7 @@
-const Map = () => {
-    return<div> Google Map goes here!</div>
+const MapComponent = ({lat,lng}) =>{
+  return(
+    <iframe src={`https://maps.google.com/maps?q=${lat},${lng}&z=15&output=embed`} title="title-test" width="500" height="270" ></iframe>
+  )
 }
 
-export default Map
+export default MapComponent
