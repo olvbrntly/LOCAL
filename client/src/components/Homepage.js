925a531d4184172fe0a84bcf989120c5b1144423
olive <olivia.brantley9@gmail.com>
2024-03-25 20:26:24 -0700

    added gunctionality for zip code form

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,8 +1,21 @@
 import { FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass } from"@fortawesome/free-solid-svg-icons";
-
+import { useState} from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Homepage = () =>{
+
+    const navigate = useNavigate();
+    const [zipCode, setZipCode] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        navigate(`/business/zipcode/${zipCode}`);
+    };
+
+
+    const onZipCodeChanged = e => setZipCode(e.target.value)
+
     return(
         <div className=" hero content">
             <div className="hero-content">
@@ -10,9 +23,19 @@ const Homepage = () =>{
                 
                 <h1 className="hero-title">Shop LOCAL</h1>
                 <p className="hero-text">Invest in your community. Find small, family owned, and local businesses near you.</p>
-                <form>
-                <input id="zip" name="zip" type="text" pattern="[0-9]*" placeholder="Zip Code"/>
-                <button className='homepage-button'><FontAwesomeIcon icon={faMagnifyingGlass} /></button>
+                <form onSubmit={handleSubmit}>
+                    <input
+                        id="zip"
+                        name="zip"
+                        type="text"
+                        pattern="[0-9]*"
+                        placeholder="Zip Code"
+                        value={zipCode}
+                        onChange={onZipCodeChanged}
+                    />
+                    <button className='homepage-button' type="submit">
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
                 </form>
 
             </div>
